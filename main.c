41666d01897dd1a33c08e2cdbc81d504c597f785
Devin Lin <dlin01@stuy.edu>
2019-11-11 23:21:31 -0500

    finished needs testing on lab computers

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 unsigned int randgen() {
   unsigned int randNumber;
-  int rand = open("/dev/random", O_RDONLY);
+  int rand = open("/dev/urandom", O_RDONLY);
   // opening file error
   if (errno) {
     printf("error #: %d error: %s\n", errno, strerror(errno));
@@ -25,6 +25,45 @@ unsigned int randgen() {
 }
 
 int main() {
-  /* code */
+  // populating the array of ints
+  unsigned int randarrayinit[10];
+  unsigned int randarraycopy[10];
+  int write_to = open("randfile.in", O_WRONLY | O_CREAT, 0666);
+  if (errno) {
+    printf("error #: %d error: %s\n", errno, strerror(errno));
+    return 0;
+  }
+  printf("Generating Random Numbers: \n" );
+  for (int i = 0; i < 10; i++) {
+    randarrayinit[i] = randgen();
+    printf("random %d: %u\n", i, randarrayinit[i]);
+  }
+
+  printf("\nWriting numbers to file...\n");
+  write(write_to, &randarrayinit, sizeof(int) * 10);
+  if (errno) {
+    printf("error #: %d error: %s\n", errno, strerror(errno));
+    return 0;
+  }
+  close(write_to);
+
+  int read_from = open("randfile.in", O_RDONLY);
+  if (errno) {
+    printf("error #: %d error: %s\n", errno, strerror(errno));
+    return 0;
+  }
+
+  printf("\nReading numbers from file...\n\n");
+  read(read_from, &randarraycopy, sizeof(int) * 10);
+  if (errno) {
+    printf("error #: %d error: %s\n", errno, strerror(errno));
+    return 0;
+  }
+  close(read_from);
+
+  printf("Verification that written values were the same:\n");
+  for (int i = 0; i < 10; i++) {
+    printf("\trandom %d: %u\n", i, randarraycopy[i]);
+  }
   return 0;
 }
